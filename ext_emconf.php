61a168b2d18a109a383bcc18f15fcc8b37a4b210
Sascha Ende <sascha@sascha-ende.de>
2019-08-21 12:03:29 +0200

    Modified   ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.6.0-9.5.5',
+            'typo3' => '8.6.0-9.9.9',
         ],
         'conflicts' => [],
         'suggests' => [],
